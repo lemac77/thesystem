f658edbb3868eda19dfff28aa37e0c43e44546d7
lemac77 <info@lucez.it>
2026-06-22 09:59:34 +0200

    Update sw.js

--- the-system/public/sw.js
+++ the-system/public/sw.js
@@ -1,10 +1,14 @@
-// No cache - always fetch from network
-self.addEventListener('install', e => self.skipWaiting());
+const CACHE = 'the-system-v1';
+const ASSETS = ['/thesystem/', '/thesystem/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
 self.addEventListener('fetch', e => {
-  // Just fetch, never cache
-  e.respondWith(fetch(e.request).catch(() => new Response('Offline', {status: 503})));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
